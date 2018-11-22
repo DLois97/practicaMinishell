997dacfc3daa5343078849ef29d5d01fc00b73d5
DLois97 <37405525+DLois97@users.noreply.github.com>
2018-11-22 18:32:58 +0100

    estilo
    Se añaden comentarios.
    Se eliminan cosas que no se llegaban a utilizar.

--- miniShell.c
+++ miniShell.c
@@ -10,39 +10,34 @@
 #include <string.h>
 #include <signal.h>
 
-tline *comandos; //Variable para introducir los comandos que entren 
-int** crear_pipes(){
+// *comandos; //Variable para introducir los comandos que entren 
+int** crear_pipes(int n){
 		int **p; //array de pipes
 		int i;
-		p = malloc(sizeof(int*)*comandos->ncommands-1);//reservamos el espacio en memoria   para abrir los pipes necesarios
-		for(i=0; i<comandos->ncommands-1; i++){
+		p = malloc(sizeof(int*)*n-1);//reservamos el espacio en memoria   para abrir los pipes necesarios
+		for(i=0; i<n-1; i++){
 			p[i]= malloc(sizeof(int)*2);
 			pipe(p[i]);
 		}
 		return p;
 }
 	
-void cerrar_pipes(int **p){
+void cerrar_pipes(int **p,int n){
 		int i;
-		for(i=0; i<comandos->ncommands-1; i++){
+		for(i=0; i<n-1; i++){
 			close(p[i][0]);
 			close(p[i][1]);
 			free(p[i]);
 		}
 		free(p);
 }
-void ejecutarComandos(void){
+void ejecutarComandos(tline *comandos){
 	int i;
 	int fd;
-	int *pids;
 	int pid;
 	int **pipes;
 	int cmd = comandos ->ncommands;
-	pids= malloc(sizeof(int)*comandos->ncommands);
-	
-
-//fin de redirección de datos>
-	pipes = crear_pipes();
+	pipes = crear_pipes(cmd);
 
 	for (i=0;i<comandos->ncommands;i++){
 		pid = fork();
@@ -50,27 +45,27 @@ void ejecutarComandos(void){
 			fprintf(stderr, "Error. %s\n", comandos->commands[i].filename);
 			exit(1);
 		}
-		pids[i]=pid;
-		if (pid == 0){
-				//<inicio de redirección de datos
-	if ((comandos->redirect_input!= NULL)&&(i==0)){
-		fd=open(comandos->redirect_input,O_RDONLY); //si solo lees "open"
-		dup2(fd,0);
-		close(fd);
+		if (pid == 0){ // el pid es 0 si es el hijo
+			//<inicio de redirección de datos
+			if ((comandos->redirect_input!= NULL)&&(i==0)){
+				fd=open(comandos->redirect_input,O_RDONLY); //si solo lees "open"
+				dup2(fd,0);
+				close(fd);
 
-	}
-	if ((comandos->redirect_output!= NULL)&&(i==cmd-1)){
-		fd=creat(comandos->redirect_output,0644);//si vas a escribir "creat"
-		dup2(fd,1);
-		close(fd);
+			}
+			if ((comandos->redirect_output!= NULL)&&(i==cmd-1)){
+				fd=creat(comandos->redirect_output,0644);//si vas a escribir "creat"
+				dup2(fd,1);
+				close(fd);
 
-	} 
-	if ((comandos->redirect_error!= NULL)&&(i==cmd-1)){
-		fd=creat(comandos->redirect_error,0644);//si vas a escribir "creat"
-		dup2(fd,2);
-		close(fd);
+			} 
+			if ((comandos->redirect_error!= NULL)&&(i==cmd-1)){
+				fd=creat(comandos->redirect_error,0644);//si vas a escribir "creat"
+				dup2(fd,2);
+				close(fd);
 
-	}	//es 0 si es el hijo
+			}
+			//fin de redirección de datos>
 			if (cmd > 1){
 				if(i==0){ //si es el primer comando ejecutando redirecciona la entrada del pipe para meter datos
 					dup2(pipes[i][1],1);
@@ -79,83 +74,23 @@ void ejecutarComandos(void){
 				} else {
 					dup2(pipes[i][1],1);
 					dup2(pipes[i-1][0],0);
+					}
+				cerrar_pipes(pipes,cmd);
 				}
-			cerrar_pipes(pipes);
-			}
-		execv(comandos->commands[i].filename,comandos->commands[i].argv);
-		fprintf(stderr, "Error. %s\n", comandos->commands[i].filename);
-		exit(1);
-		}
-	}
-	cerrar_pipes(pipes);
-}
-	/*pipes = crear_pipes();
-	for(i=0; i<comandos->ncommands; i++){
-		pid= fork();
-		if(pid<0){
-			fprintf(stderr,"Error. fork\n");
-			exit(1);
-		}
-		pids[i]=pid;
-		if(pid==0){
-			if(!comandos->background){
-				signal(SIGINT, SIG_DFL);
-				signal(SIGQUIT, SIG_DFL);
-			}
-			if((i==0) && (comandos->redirect_input!=NULL)){
-				fd=open(comandos->redirect_input,O_RDONLY);
-				if(fd<0){
-					fprintf(stderr,"Error.%s \n",comandos->redirect_input);
-					exit(1);
-				}
-				dup2(fd,0);
-				close(fd);
-			}
-			if((i==comandos->ncommands-1) && (comandos->redirect_output!=NULL)){
-				fd=creat(comandos->redirect_output,0644);
-				if(fd<0){
-					fprintf(stderr,"Error.%s \n",comandos->redirect_output);
-					exit(1);
-				}
-				dup2(fd,1);
-				close(fd);
-			}
-			if((i==comandos->ncommands-1) && (comandos->redirect_error!=NULL)){
-				fd=creat(comandos->redirect_error,0644);
-				if(fd<0){
-					fprintf(stderr,"Error.%s /n",comandos->redirect_error);
-					exit(1);
-				}
-				dup2(fd,2);
-				close(fd);
-			}
-			if(cmd>1){
-				if(i==0)
-		    		dup2(pipes[i][1],1);
-				else if(i==cmd-1){
-					dup2(pipes[i-1][0],0);
-				}
-				else{
-					dup2(pipes[i][1],1);
-					dup2(pipes[i-1][0],0);
-
-				}
-				cerrar_pipes(pipes);
-			}
 			execv(comandos->commands[i].filename,comandos->commands[i].argv);
 			fprintf(stderr, "Error. %s\n", comandos->commands[i].filename);
 			exit(1);
 		}
+	}
+	cerrar_pipes(pipes,cmd);
 }
-cerrar_pipes(pipes);
-}
-*/
 int main(void) {
 	char buffer[1024];
+	tline *comandos;
 	while (!feof(stdin)){
 		printf("msh> ");
 		fgets(buffer,1024,stdin);
 		comandos = tokenize(buffer);
-		ejecutarComandos();
+		ejecutarComandos(comandos);
 	}
 }
